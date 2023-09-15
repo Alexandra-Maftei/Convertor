73b8990cece56e4e71e77e846e701dac0284e309
Alexandra-Maftei <gmafteialexandra@gmail.com>
2023-09-15 15:05:42 +0300

    Add project files.

---
+++ Tema4/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Tema4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.heightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.weightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temperatureToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.comboBoxIntrare = new System.Windows.Forms.ComboBox();
+            this.comboBoxIesire = new System.Windows.Forms.ComboBox();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.heightToolStripMenuItem,
+            this.weightToolStripMenuItem,
+            this.temperatureToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // heightToolStripMenuItem
+            // 
+            this.heightToolStripMenuItem.Name = "heightToolStripMenuItem";
+            this.heightToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.heightToolStripMenuItem.Text = "Lungime";
+            this.heightToolStripMenuItem.Click += new System.EventHandler(this.lengthToolStripMenuItem_Click);
+            // 
+            // weightToolStripMenuItem
+            // 
+            this.weightToolStripMenuItem.Name = "weightToolStripMenuItem";
+            this.weightToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.weightToolStripMenuItem.Text = "Greutate";
+            this.weightToolStripMenuItem.Click += new System.EventHandler(this.weightToolStripMenuItem_Click);
+            // 
+            // temperatureToolStripMenuItem1
+            // 
+            this.temperatureToolStripMenuItem1.Name = "temperatureToolStripMenuItem1";
+            this.temperatureToolStripMenuItem1.Size = new System.Drawing.Size(107, 24);
+            this.temperatureToolStripMenuItem1.Text = "Temperatură";
+            this.temperatureToolStripMenuItem1.Click += new System.EventHandler(this.temperatureToolStripMenuItem1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // comboBoxIntrare
+            // 
+            this.comboBoxIntrare.FormattingEnabled = true;
+            this.comboBoxIntrare.Location = new System.Drawing.Point(96, 73);
+            this.comboBoxIntrare.Name = "comboBoxIntrare";
+            this.comboBoxIntrare.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxIntrare.TabIndex = 2;
+            // 
+            // comboBoxIesire
+            // 
+            this.comboBoxIesire.FormattingEnabled = true;
+            this.comboBoxIesire.Location = new System.Drawing.Point(439, 73);
+            this.comboBoxIesire.Name = "comboBoxIesire";
+            this.comboBoxIesire.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxIesire.TabIndex = 3;
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxValue.Location = new System.Drawing.Point(303, 182);
+            this.textBoxValue.Multiline = true;
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(136, 30);
+            this.textBoxValue.TabIndex = 4;
+            this.textBoxValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConvert.Location = new System.Drawing.Point(570, 222);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(110, 39);
+            this.buttonConvert.TabIndex = 7;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Valoarea introdusa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(113, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Rezultatul";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(318, 285);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 20);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(570, 285);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(110, 39);
+            this.buttonClear.TabIndex = 10;
+            this.buttonClear.Text = "Stergere";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.comboBoxIesire);
+            this.Controls.Add(this.comboBoxIntrare);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem heightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem weightToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ComboBox comboBoxIntrare;
+        private System.Windows.Forms.ComboBox comboBoxIesire;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.ToolStripMenuItem temperatureToolStripMenuItem1;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Tema4/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema4
+{
+    public partial class Form1 : Form
+    {
+        private string[] lengthUnits = { "Metri", "Inch", "Feet", "Kilometri" };
+        private string[] weightUnits = { "Kilograme", "Grame", "Uncii", "Livre" };
+        private string[] temperatureUnits = { "Celsius", "Kelvin", "Fahrenheit" };
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxIntrare.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxIesire.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult exit = MessageBox.Show("Doriți să închideți aplicația?","Exit",MessageBoxButtons.YesNo);
+           // this.Close();
+        }
+
+        private void lengthToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comboBoxIntrare.Items.Clear();
+            comboBoxIesire.Items.Clear();
+            comboBoxIntrare.Items.AddRange(lengthUnits);
+            comboBoxIesire.Items.AddRange(lengthUnits);
+        }
+
+        private void weightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comboBoxIntrare.Items.Clear();
+            comboBoxIesire.Items.Clear();
+            comboBoxIntrare.Items.AddRange(weightUnits);
+            comboBoxIesire.Items.AddRange(weightUnits);
+        }
+
+        private void temperatureToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            comboBoxIntrare.Items.Clear();
+            comboBoxIesire.Items.Clear();
+            comboBoxIntrare.Items.AddRange(temperatureUnits);
+            comboBoxIesire.Items.AddRange(temperatureUnits);
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            double value;
+            if (!double.TryParse(textBoxValue.Text, out value))
+            {
+                MessageBox.Show("Introduceți o valoare numerică validă!", "Eroare",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string InputUnit = comboBoxIntrare.SelectedItem.ToString();
+            string OutputUnit = comboBoxIesire.SelectedItem.ToString();
+
+            double convertedValue = 0;
+
+            switch (InputUnit)
+            {
+                case "Metri":
+                    switch (OutputUnit)
+                    {
+                        case "Metri":
+                            convertedValue = value;
+                            break;
+                        case "Inch":
+                            convertedValue = value * 39.37;
+                            break;
+                        case "Feet":
+                            convertedValue = value * 3.281;
+                            break;
+                        case "Kilometri":
+                            convertedValue = value / 1000;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Inch":
+                    switch (OutputUnit)
+                    {
+                        case "Inch":
+                            convertedValue = value;
+                            break;
+                        case "Metri":
+                            convertedValue = value / 39.37;
+                            break;
+                        case "Feet":
+                            convertedValue = value / 12;
+                            break;
+                        case "Kilometri":
+                            convertedValue = value / 39370.079;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Feet":
+                    switch(OutputUnit)
+                    {
+                        case "Feet":
+                            convertedValue = value;
+                            break;
+                        case "Metri":
+                            convertedValue = value / 3.281;
+                            break;
+                        case "Inch":
+                            convertedValue = value * 12;
+                            break;
+                        case "Kilometri":
+                            convertedValue = value / 3280.84;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Kilometri":
+                    switch (OutputUnit)
+                    {
+                        case "Kilometri":
+                            convertedValue = value;
+                            break;
+                        case "Metri":
+                            convertedValue = value * 1000;
+                            break;
+                        case "Inch":
+                            convertedValue = value * 39370.079;
+                            break;
+                        case "Feet":
+                            convertedValue = value * 3280.84;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Kilograme":
+                    switch (OutputUnit)
+                    {
+                        case "Kilograme":
+                            convertedValue = value;
+                            break;
+                        case "Grame":
+                            convertedValue = value * 1000;
+                            break;
+                        case "Uncii":
+                            convertedValue = value * 35.274;
+                            break;
+                        case "Livre":
+                            convertedValue = value * 2.205;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Grame":
+                    switch (OutputUnit)
+                    {
+                        case "Grame":
+                            convertedValue = value;
+                            break;
+                        case "Kilograme":
+                            convertedValue = value / 1000;
+                            break;
+                        case "Uncii":
+                            convertedValue = value * 0.035274;
+                            break;
+                        case "Livre":
+                            convertedValue = value * 0.002205;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Uncii":
+                    switch (OutputUnit)
+                    {
+                        case "Uncii":
+                            convertedValue = value;
+                            break;
+                        case "Kilograme":
+                            convertedValue = value * 0.02835;
+                            break;
+                        case "Grame":
+                            convertedValue = value * 28.35;
+                            break;
+                        case "Livre":
+                            convertedValue = value * 0.0625;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Livre":
+                    switch (OutputUnit)
+                    {
+                        case "Livre":
+                            convertedValue = value;
+                            break;
+                        case "Kilograme":
+                            convertedValue = value * 0.4536;
+                            break;
+                        case "Grame":
+                            convertedValue = value * 453.6;
+                            break;
+                        case "Uncii":
+                            convertedValue = value * 16;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Celsius":
+                    switch (OutputUnit)
+                    {
+                        case "Celsius":
+                            convertedValue = value;
+                            break;
+                        case "Kelvin":
+                            convertedValue = value + 273.15;
+                            break;
+                        case "Fahrenheit":
+                            convertedValue = (value * 9 / 5) + 32;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Kelvin":
+                    switch (OutputUnit)
+                    {
+                        case "Kelvin":
+                            convertedValue = value;
+                            break;
+                        case "Celsius":
+                            convertedValue = value - 273.15;
+                            break;
+                        case "Fahrenheit":
+                            convertedValue = (value - 273.15) * 9 / 5 + 32;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                case "Fahrenheit":
+                    switch (OutputUnit)
+                    {
+                        case "fahrenheit":
+                            convertedValue = value;
+                            break;
+                        case "Celsius":
+                            convertedValue = (value - 32) * 5 / 9;
+                            break;
+                        case "Kelvin":
+                            convertedValue = (value - 32) * 5 / 9 + 273.15;
+                            break;
+                        default:
+                            MessageBox.Show("Unitatea de ieșire nu este validă!", "Eroare",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Unitatea de intrare nu este validă!", "Eroare",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+            }
+
+
+                    labelResult.Text=$"{convertedValue}";
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            comboBoxIntrare.SelectedIndex = -1;
+            comboBoxIesire.SelectedIndex = -1;
+            textBoxValue.Clear();
+            labelResult.Text = string.Empty;
+            MessageBox.Show("Datele au fost sterse cu succes!", "Stergere",
+                        MessageBoxButtons.OK);
+        }
+    }
+}
